40303578ebf1a20f538e7fff204afa0cd25ec9a4
Gokhan Ozgozen <gokhan.ozgozen@gmail.com>
2026-08-24 18:01:08 +0300

    Output rule definition and tests.

---
+++ qplan/build-logic/detekt-rules/src/main/kotlin/detekt/EngineDataRuleSetProvider.kt
@@ -0,0 +1,20 @@
+package detekt
+
+import io.gitlab.arturbosch.detekt.api.Config
+import io.gitlab.arturbosch.detekt.api.RuleSet
+import io.gitlab.arturbosch.detekt.api.RuleSetProvider
+
+/**
+ * Provides the engine data Detekt rules to the Detekt framework.
+ */
+class EngineDataRuleSetProvider : RuleSetProvider {
+    override val ruleSetId: String = "engine-data-rules"
+
+    override fun instance(config: Config) =
+        RuleSet(
+            ruleSetId,
+            listOf(
+                EngineOutputDataOverrideRule(config)
+            )
+        )
+}
